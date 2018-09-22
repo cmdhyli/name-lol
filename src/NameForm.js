aea6ebe8c91259a9297d0884130ee0bf52e8aad1
Welson Pan <welson.pan@hotmail.com>
2018-09-22 15:50:01 -0400

    Added NameList component

--- src/NameForm.js
+++ src/NameForm.js
@@ -1,32 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
+import NameList from './NameList'
 
 class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: '', placeholder: 'Enter a name'};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            placeholder: 'Enter a name',
+            names: [],
+        };
+    }
 
-  handleChange = (event) => {
-    this.setState({value: event.target.value});
-  }
+    handleChange = (event) => {
+        this.setState({
+            ...this.state,
+            value: event.target.value
+        });
+    }
 
-  handleSubmit = (event) => {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
+    handleSubmit = (event) => {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+        this.setState({
+            ...this.state,
+            names: ['xd', 'hi']
+        });
+    }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
 
-  <input type="text" value={this.state.value} onChange={this.handleChange}  placeholder={this.state.placeholder}/>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+                        <input type="text" value={this.state.value} onChange={this.handleChange}  placeholder={this.state.placeholder}/>
+                    </label>
+                    <input type="submit" value="Submit" />
+                </form>
+                <NameList names={this.state.names} />
+            </div>
+        );
+    }
 }
 
 export default NameForm;
